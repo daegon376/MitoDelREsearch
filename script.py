4cd736f954b32ea50c5530861f8925ff169a127c
daegon376 <daegon376@yandex.ru>
2020-10-01 19:43:42 +0200

    01.10.2020

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import os
-import re
 
 cwd = os.getcwd()
 input_path = str(cwd + '\\refseq\\modified_refseq.fasta')
@@ -20,11 +19,10 @@ seq = full_seq  # [fragment_start:fragment_end]
 # print('Последовательность в работе: ' + seq)
 print('Длина исследуемой области: ' + str(len(seq)))
 
-del_length_min = int(1)  # input('Минимальная длина делеции: '))
-del_length_max = int(20)  # input('Максимальная длина делеции: '))
-marg_length = int(25)  # input('Длина "боков": '))
-mask_length = int(10)  # input('Длина маскирования нуклеотидов = половина шага по длинам делеций \n'
-                       # '(ноль -> выкл. -> шаг в 1 нукл): '))
+del_length_min = int(input('Минимальная длина делеции: '))
+del_length_max = int(input('Максимальная длина делеции: '))
+marg_length = int(input('Длина "боков": '))
+mask_length = int(input('Длина маскирования нуклеотидов = половина шага по длинам делеций \n (ноль -> выкл. -> шаг в 1 нукл): '))
 both_strands = str(input('генерировать для одной цепи (+) или для двух (+-)? '))
 
 if mask_length == 0:  # шаг по делециям != 0
@@ -37,8 +35,8 @@ start_time = datetime.now()  # запуск таймера
 def complementary_conversion(line):
     new_line = str('')
     complementary_list = [['A', 'T'], ['T', 'A'], ['G', 'C'], ['C', 'G'],
-                               ['M', 'K'], ['K', 'M'], ['R', 'Y'], ['Y', 'R'], ['W', 'W'], ['S', 'S'],
-                               ['B', 'V'], ['V', 'B'], ['H', 'D'], ['D', 'H']]
+                          ['M', 'K'], ['K', 'M'], ['R', 'Y'], ['Y', 'R'], ['W', 'W'], ['S', 'S'],
+                          ['B', 'V'], ['V', 'B'], ['H', 'D'], ['D', 'H']]
     for letter in line:
         for a in complementary_list:
             if letter == a[0]:
@@ -58,14 +56,16 @@ def reg_exp_generator(sequence, deletion_length, margin_length=30, masking=10):
         left_arm = str(sequence[reg_exp_start:deletion_start - masking])
         right_arm = str(sequence[deletion_end + masking:reg_exp_end])
         for direction in ['+', '-']:
+            if both_strands == '+' and direction == '-':
+                break
+            elif both_strands == '+-' and direction == '-':
+                larm = left_arm
+                left_arm = complementary_conversion(right_arm[::-1])
+                right_arm = complementary_conversion(larm[::-1])
             del_info = str('del_start {0}; del_end {1}; {2} {3} re:'.format(str(deletion_start - masking),
                                                                             str(deletion_end + masking),
                                                                             str(direction),
                                                                             str(deletion_length + 2 * masking)))
-            if direction == '-' and both_strands == '+-':
-                larm = left_arm
-                left_arm = complementary_conversion(right_arm[::-1])
-                right_arm = complementary_conversion(larm[::-1])
 
             reg_exp_seq = left_arm + '([ATGC]{0,' + str(masking * 2) + '})' + right_arm + '\n'
 
