6bcb5ace3227d3f9bcf5a71aeba9640538ca5046
daegon376 <59346018+daegon376@users.noreply.github.com>
2020-10-09 17:42:19 +0200

    Add files via upload

--- REvsNUC.py
+++ REvsNUC.py
@@ -3,27 +3,25 @@ import re
 import os
 
 start_time = datetime.now()
-cwd = str(os.getcwd())
-nuc_seq_path = str(cwd + '\\refseq\\genome.txt')
+cwd = str(os.getcwd())  # берем путь к текущей папке (где лежит скрипт)
+nuc_seq_path = str(cwd + '\\refseq\\genome.txt')  # путь к хуман геному без переноса строк
 
 with open('RE.txt') as re_file:
-    x = int(1)
-    for re_line in re_file:
-        with open(nuc_seq_path, 'r') as genome_file:
+        for re_line in re_file:
+        with open(nuc_seq_path, 'r') as genome_file:  # открыли хумангеном на чтение
             calls = int(0)
-            for line in genome_file:
-                if not line.startswith('>') and line != '\n':
+            for line in genome_file:  # итерируем строки в файле хуман генома
+                if not line.startswith('>') and line != '\n':  # если строка НЕ заголовок и НЕ состоит
+                                                               # из одного переноса, то это строка с хромосомной
+                                                               # последовательностью, дальше делаем с ней что хотим
                     regexp = re.split(r're:', re_line)[1][:-1]
                     calls += len(re.findall(regexp, line))
 
-        print('время: ' + str(datetime.now() - start_time))
         print(str(x) + '. calls: ' + str(calls) + '. ' + re_line)
         with open('log_CDREvsNUC.txt', 'a') as output_file:
             output_file.write(str(x) + '. calls: ' + str(calls) + '. ' + re_line)
         if calls >> 1:
             print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        x += 1
-
-
+        
 
 print('Полное время: ' + str(datetime.now() - start_time))
