71d58cc6be11edcb721f0f3d1cf14df81ad6c47c
daegon376 <59346018+daegon376@users.noreply.github.com>
2020-10-09 18:35:56 +0200

    Add files via upload

--- REsearch_multip.py
+++ REsearch_multip.py
@@ -4,12 +4,14 @@ from datetime import datetime
 import re
 from itertools import islice
 
+
 def split_every(n, iterable):
     i = iter(iterable)
     piece = list(islice(i, n))
     while piece:
         yield piece
         piece = list(islice(i, n))
+
 
 def re_search(re_list, reads):
     for s in re_list:
@@ -20,7 +22,8 @@ def re_search(re_list, reads):
         deletions = pattern.findall(reads)
         if len(deletions) != 0:  # если что-то нашли
             new_reg_exp = str('.*' + str(re.split(r'\W\WATGCN\W\W0.\d{1,3}\W\W', reg_exp)[0]) + deletions[0] +
-                              str(re.split(r'\W\WATGCN\W\W0.\d{1,3}\W\W', reg_exp)[1]) + '.*')  # регулярка для поиска ридов с делецией
+                              str(re.split(r'\W\WATGCN\W\W0.\d{1,3}\W\W', reg_exp)[
+                                      1]) + '.*')  # регулярка для поиска ридов с делецией
             reads_with_del = re.findall(new_reg_exp, reads)
             amount_of_finded_reads = str(len(reads_with_del))
             deletion_length = str(int(re.search(r'\d*$', info).group(0)) - len(deletions[0]))  # вычисляем длину делеции
@@ -31,29 +34,28 @@ def re_search(re_list, reads):
                       '\n    Amount of calls:', len(deletions),
                       '\nReads with deletion:', amount_of_finded_reads, reads_with_del, file=output)
 
+
 if __name__ == '__main__':
     start_time = datetime.now()  # запуск таймера
-
     cwd = os.getcwd()
+    number_of_procecces = int(8)  # сколько процессов будет процессов. оставь 8, т.к. регулярок в 'RECDmin.txt' 8 шт
 
-    reg_exp_file = open('RECDmin.txt')
-    reads_file = open(cwd + '\\control\\test_output_reads.txt')  # \\refseq\\mtDNA +-strands with DmtDNA4977.txt')
+    reg_exp_file = open('RECDmin.txt')  # файл с регулярками
+    reads_file = open(cwd + '\\control\\test_output_reads.txt')  # файл с ридами
     f = open('REsults_multi.txt', 'w')
     f.write('')  # clean output
-    reads = str()  # все риды записываем в одну строку
+    reads = str()  # все риды записываем в одну строку !!! тут могут быть проблемы со скоростью !!!
     for s in reads_file:
         reads = str(reads + s)
 
-    n = int(0)
-    reg_exp_l = []
-    for s in reg_exp_file:
-        reg_exp_l.append(s)
-        n += 1
+    reg_exp_list = []
+    for s in reg_exp_file:  # записываем регулярки в список, чтобы поделить поровну на процессы
+        reg_exp_list.append(s)
 
-    number_to_split = (n//8) + 1
-    splited_re = list(split_every(number_to_split, reg_exp_l))
+    number_to_split = (len(reg_exp_list) // number_of_procecces)
+    splited_re = list(split_every(number_to_split, reg_exp_list))  # делим список регулярок
 
-    procs = []
+    procs = []  # наши процеессы
 
     for re_list in splited_re:
         proc = Process(target=re_search, args=(re_list, reads))
